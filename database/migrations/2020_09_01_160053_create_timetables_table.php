5cdadfc25be291700c0445e6536dd9c7fee0f50b
Димас <dimadetected@yandex.ru>
2020-09-01 19:11:34 +0300

    Модели и миграции

---
+++ database/migrations/2020_09_01_160053_create_timetables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('type')->nullable();
+            $table->integer('group_id')->nullable();
+            $table->integer('faculty_id')->nullable();
+            $table->integer('course_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetables');
+    }
+}
