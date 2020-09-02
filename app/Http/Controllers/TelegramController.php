09913fd4b78a123d8368c561391a9d3ae4937477
Димас <dimadetected@yandex.ru>
2020-09-02 20:26:19 +0300

    телега бот

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Request;
 use Telegram\Bot\Api;
 
 class TelegramController extends Controller
 {
     protected $telegram;
-    
+    protected $chat_id;
+    protected $username;
+    protected $text;
     public function __construct()
     {
         $this->telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
@@ -24,5 +28,46 @@ class TelegramController extends Controller
         $response = $this->telegram->setWebhook(['url' => $url]);
         
         return $response == true ? redirect()->back() : dd($response);
+    }
+    public function handleRequest(Request $request)
+    {
+        $this->chat_id = $request['message']['chat']['id'];
+        $this->username = $request['message']['from']['username'];
+        $this->text = $request['message']['text'];
+        
+        switch ($this->text) {
+            case '/start':
+            case '/menu':
+            default:
+                $this->showMenu();
+        }
+    }
+    
+    public function showMenu($info = null)
+    {
+        $message = '';
+        if ($info) {
+            $message .= $info . chr(10);
+        }
+        $message .= '/menu' . chr(10);
+        $message .= '/getGlobal' . chr(10);
+        $message .= '/getTicker' . chr(10);
+        $message .= '/getCurrencyTicker' . chr(10);
+        
+        $this->sendMessage($message);
+    }
+    
+    
+    
+    protected function sendMessage($message, $parse_html = false)
+    {
+        $data = [
+            'chat_id' => $this->chat_id,
+            'text' => $message,
+        ];
+        
+        if ($parse_html) $data['parse_mode'] = 'HTML';
+        
+        $this->telegram->sendMessage($data);
     }
 }
