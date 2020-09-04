b620be114452f9a9df114b6d50b76d3b17166d05
Димас <dimadetected@yandex.ru>
2020-09-04 10:08:14 +0300

    кнопки

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -51,7 +51,6 @@ class TelegramController extends Controller
             'password' => bcrypt(1),
         ]);
         file_put_contents(public_path('request.json'), json_encode($request['message']));
-        $this->sendMessage(json_encode($request['callback_query']['data']));
         $this->sendMessage(json_encode(array_keys($request)));
     
         
