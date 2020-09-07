dbc757be70103fc9d28b1f4b67770631f0b90f04
Димас <dimadetected@yandex.ru>
2020-09-07 22:25:28 +0300

    кнопки

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -55,6 +55,8 @@ class TelegramController extends Controller
         $this->username = $request['message']['from']['username'];
         $this->text = $request['message']['text'];
         
+        $this->sendMessage('123');
+        
         $user = \App\User::query()->firstOrCreate([
             'tg_id' => $this->chat_id,
         ], [
