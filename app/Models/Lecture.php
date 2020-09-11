74b5b40ae98c8103b3281f9185830ed4cb56dc99
Дима <dimadetected@yandex.ru>
2020-09-11 09:53:01 +0300

    группы для предмета

--- app/Models/Lecture.php
+++ app/Models/Lecture.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Lecture extends Model
 {
     protected $guarded = ['id'];
-    
+
+    public function group()
+    {
+        return $this->belongsTo('App\Models\Group');
+    }
 }
